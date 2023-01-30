3dc5b43e2993f39ac09bcf21b8cf99bf508426e8
Ian Herrington <ianherri20@gmail.com>
2023-01-30 16:33:03 -0600

    change firestore rules

--- functions/index.js
+++ functions/index.js
@@ -4,18 +4,15 @@ const admin = require('firebase-admin')
 
 admin.initializeApp()
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
+// sample endpoint at http://127.0.0.1:5001/songduo-5425e/us-central1/addMessage?text=sometext
+// where text is query param
 exports.addMessage = functions.https.onRequest(async (req, res) => {
   // Grab the text parameter.
   const original = req.query.text
   // Push the new message into Firestore using the Firebase Admin SDK.
   const writeResult = await admin
     .firestore()
-    .collection('messages')
+    .collection('test-admin')
     .add({ original: original })
   // Send back a message that we've successfully written the message
   res.json({ result: `Message with ID: ${writeResult.id} added.` })
